3c372dd62f31bf28f66d95d1ec2434b0843176f9
zwedwin <95667346+zwedwin@users.noreply.github.com>
2022-01-21 10:26:44 -0500

    Update WRAP_POSTER.py

--- WRAP_POSTER.py
+++ WRAP_POSTER.py
@@ -22,13 +22,14 @@ class WRAP_POSTER:
         self.driver.implicitly_wait(10)
         self.driver.execute_script("window.open()")
         self.driver.execute_script("window.open()")
-        self.WQ = WRAP_Queue('WRAPS_TO_POST', 'WRAPS_POSTED')
+        self.WQ = WRAP_Queue()
         self.WRAP_INFO = self.WQ.WRAP_info
         self.pm_user = pm_user
         self.pm_pass = pm_pass
         self.MD = McNulty(pm_user, pm_pass, self.driver, self.driver.window_handles[0])
         self.CC = CoreCommerce(cc_user, cc_pass, self.driver, self.driver.window_handles[1])
         self.DACIS = DACIS(DACIS_user, DACIS_pass, self.driver, self.driver.window_handles[2])
+
 
     def post_product(self):
         #Step 1: Find and select product on primemover, get info
@@ -39,6 +40,8 @@ class WRAP_POSTER:
         self.WRAP_INFO['Product Name'] = self.MD.get_Product_Name()
         self.WRAP_INFO['SKU'] = self.MD.get_SKU()
         self.WRAP_INFO['Cost_Center'] = self.MD.get_Cost_Center()
+        self.WRAP_INFO['City'] = self.MD.get_city()
+        self.WRAP_INFO['State'] = self.MD.get_state()
 
         #Step 2: Post product info to core CoreCommerce
         self.driver.switch_to.window(self.driver.window_handles[1])
@@ -55,10 +58,10 @@ class WRAP_POSTER:
         #Step 4: Set prime mover link and date and save
         self.driver.switch_to.window(self.driver.window_handles[0])
         self.MD.set_Product_Info(self.WRAP_INFO['Search Tag'])
-        u_input = bool(input('Post WRAP?'))
-        if(input):
+        u_input = input('Post WRAP?')
+        if(u_input == "yes"):
             self.MD.post_WRAP()
-            
+
         #Step 5: Save and Close excel file
         self.WQ.workbook.save(self.WQ.current_wrap)
         self.WQ.workbook.close()
@@ -67,5 +70,6 @@ class WRAP_POSTER:
 
 
 if __name__ == '__main__':
-    wp = WRAP_POSTER()
-    wp.post_product()
+    wp = WRAP_POSTER('jlauretti','L@ur3tt1Jul13n!','Zach','Fearful4jesuit!','mmcnulty','mcnulty2015')
+    while True:
+        wp.post_product()
